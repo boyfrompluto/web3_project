0a358aa8ddcbfd3f8f65741f120d7d7389e5029c
Charles-David Ondoma <charlesdavidondoma@gmail.com>
2023-10-14 19:45:01 -0700

    first commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import { HiMenuAlt4 } from "react-icons/hi"
+import {AiOutlineClose} from "react-icons/ai"
+import logo from "../../images/logo.png"
+const Navbar=()=>{
+    return(
+        <nav className="w-full flex md:justify-center justify-between items-center p-4">
+            <div className="md:flex-[0.5] flex-initial justify-center items-center">
+                <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+            </div>
+            <ul>
+                
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
